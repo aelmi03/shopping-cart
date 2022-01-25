cb09f5af90b8cd99418670c7ea5ee49033d8557c
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2022-01-24 17:41:42 -0800

    Created my GlobalStyle and my Theme object which I will pass to ThemeProvider

---
+++ src/Themes/Theme.js
@@ -0,0 +1,14 @@
+const Theme = {
+  colors: {
+    primary: "#263238",
+    primaryLight: "#4f5b62",
+    primaryDark: "#000a12",
+    secondary: "#9ccc65",
+    secondaryLight: "#cfff95",
+    secondaryDark: "#6b9b37",
+    primaryText: "#FFFFFF",
+    secondaryText: "#000000",
+  },
+};
+
+export default Theme;
