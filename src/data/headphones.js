00fc80ebc0a4b4cd8d917d1e69cc49688f0f4cd7
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2022-01-26 20:40:58 -0800

    Added my headphones data containg 10 headphones objects

---
+++ src/data/headphones.js
@@ -0,0 +1,86 @@
+import uniqid from "uniqid";
+
+const headphones = [
+  {
+    id: uniqid(),
+    name: "Soundcore Life Tune Over-Ear Noise Cancelling Bluetooth Headphones - Grey",
+    price: 99.99,
+    description:
+      "To enjoy music on the go, opt for these Soundcore Life Tune over-ear Bluetooth headphones with Hi-Res certified sound. Its hybrid active noise cancellation technology cuts off background noise during calls or while listening to music. Additionally, the transport, indoor, and outdoor modes help block specific noises more effectively.",
+    imgSrc: "https://m.media-amazon.com/images/I/61+WYAjltpL._AC_SL1500_.jpg",
+  },
+  {
+    id: uniqid(),
+    name: "Sennheiser HD 458BT Over-Ear Noise Cancelling Bluetooth Headphones - Black",
+    price: 129.99,
+    description:
+      "Listen to your favourite beats with dynamic and detailed audio with the Sennheiser over-hear noise cancelling headphones. Featuring a minimalist design and a battery life of up to 30 hours, it is an excellent accessory for music junkies. It also comes with a virtual assistant button for voice interaction with Siri and Google Assistant.",
+    imgSrc: "https://m.media-amazon.com/images/I/71z97HfSZxS._AC_SL1500_.jpg",
+  },
+  {
+    id: uniqid(),
+    name: "Bose QuietComfort 35 II Over-Ear Noise Cancelling Bluetooth Headphones - Black",
+    price: 399.99,
+    description:
+      "Designed to become part of your life, the Bose QuietComfort 35 II wireless headphones keep you immersed in amazing audio. With noise-cancelling technology and up to 20 hours of battery life, their built-in Google Assistant and Amazon Alexa let you use your voice to control your music, get directions, locate nearby restaurants, and much more.",
+    imgSrc: "https://m.media-amazon.com/images/I/81+jNVOUsJL._AC_SL1500_.jpg",
+  },
+  {
+    id: uniqid(),
+    name: "Sony WH-1000XM4 Over-Ear Noise Cancelling Bluetooth Headphones - Black",
+    price: 499.99,
+    description:
+      "Get the personalised listening experience you deserve with the Sony WH-1000XM4 over-ear Bluetooth headphones. Adaptive noise cancellation with Dual Noise Sensor and powerful Edge-AI technology combine to bring you an intelligent, premium audio experience. The built-in Google Assistant and Amazon Alexa let you control your music hands-free with voice controls.",
+    imgSrc: "https://m.media-amazon.com/images/I/71o8Q5XJS5L._AC_SL1500_.jpg",
+  },
+  {
+    id: uniqid(),
+    name: "Altec Lansing Whisper ANC Over-Ear Noise Cancelling Bluetooth Headphones - Black",
+    price: 79.99,
+    description:
+      "Listen to your favourite tunes in comfort with the Altec Lansing Whisper ANC Bluetooth Headphones. Their active noise cancelling technology filters out ambient noise for rich, clear audio. These headphones also feature up to 14 hours of battery life and a wireless range of up to 30 feet. Their foldable design offers easy portability and travel-friendly usage.",
+    imgSrc: "https://m.media-amazon.com/images/I/71W-xUVPSoL._AC_SL1500_.jpg",
+  },
+  {
+    id: uniqid(),
+    name: "Apple AirPods Pro In-Ear Noise Cancelling Truly Wireless Headphones - White",
+    price: 279.99,
+    description:
+      "AirPods Pro have been designed to deliver Active Noise Cancellation for immersive sound. Their Transparency mode lets you hear your surroundings and a customisable fit for all-day comfort keeps your tunes playing. Just like AirPods, AirPods Pro connect magically to your Apple devices. Plus, they’re ready to use straight out of the case.",
+    imgSrc: "https://m.media-amazon.com/images/I/71zny7BTRlL._AC_SL1500_.jpg",
+  },
+  {
+    id: uniqid(),
+    name: "Beats By Dr. Dre Studio Buds In-Ear Noise Cancelling Truly Wireless Headphones - Black",
+    price: 179.99,
+    description:
+      "Take full control of the music you hear in any environment with the Beats by Dr. Dre Studio Buds in-ear truly wireless headphones. Truly wireless design and customisable fit let you listen to music or answer calls without holding back. Thanks to the active noise cancelling technology, these headphones filter out external noises, so you can enjoy music or take calls without disturbance.",
+    imgSrc: "https://m.media-amazon.com/images/I/51bRSWrEc7S._AC_SL1500_.jpg",
+  },
+  {
+    id: uniqid(),
+    name: "Beats by Dr. Dre Studio3 Skyline Over-Ear Noise Cancelling Bluetooth Headphones - Midnight Black",
+    price: 399.99,
+    description:
+      "Enjoy a premium listening experience and complete wireless freedom with the Beats Studio3 over-ear headphones. Pure Adaptive Noise Canceling (Pure ANC) actively blocks unwanted external noise, while real-time audio calibration preserves the clarity, range, and emotion in your favourite music. These headphones are truly a one of a kind experience, and are undoubtedly the best headphones on the market",
+    imgSrc: "",
+  },
+  {
+    id: uniqid(),
+    name: "Jabra Elite 85h Over-Ear Noise Cancelling Bluetooth Headphones - Black - Only at Best Buy",
+    price: 349.99,
+    description:
+      "Jabra Elite 85h feature SmartSound technology, which automatically adapts to your surroundings, either turning on Smart Active Noise Cancellation (ANC) or enabling HearThrough. Personalize settings with the Jabra Sound+ app, customize music equalizer, and get 36 hours (with ANC on) or 41 hours (ANC off) of battery, as well as one-touch access to Amazon Alexa, Siri, and Google Assistant.",
+    imgSrc: "https://m.media-amazon.com/images/I/819iDDsPmWL._AC_SL1500_.jpg",
+  },
+  {
+    id: uniqid(),
+    name: "Jabra Elite Active 75t In-Ear Active Noise Cancelling Truly Wireless Headphones - Titanium",
+    price: 169.99,
+    description:
+      "Enjoy superior sound during your intense workouts with these Jabra Elite Active 75t truly wireless headphones. These comfortable in-ear headphones have active noise cancellation (ANC) to deliver fantastic sound. Plus, their IP57 waterproof rating means they stand up to intense activity with long-lasting batteries to keep you going without interruptions.",
+    imgSrc: "https://m.media-amazon.com/images/I/718bsUa11DL._AC_SL1500_.jpg",
+  },
+];
+
+export default headphones;
