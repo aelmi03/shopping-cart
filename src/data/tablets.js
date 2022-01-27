8f97611b405f457eba1cc64e55bd2e53f255a9e6
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2022-01-26 19:30:33 -0800

    Added my tablets data which contains around 10 tablet objects

---
+++ src/data/tablets.js
@@ -0,0 +1,86 @@
+import uniqid from "uniqid";
+
+const tablets = [
+  {
+    id: uniqid(),
+    name: 'Apple iPad 10.2" 64GB with Wi-Fi (9th Generation) - Silver',
+    price: 429.99,
+    description:
+      "The Apple iPad combines a beautiful 10.2-inch Retina display and tremendous capability with unmatched versatility and ease of use. And with the powerful A13 Bionic chip, an Ultra Wide front camera with Center Stage, support for Apple Pencil, and the amazing new things you can do with iPadOS 15, there’s even more to love about this iPad.",
+    imgSrc: "https://m.media-amazon.com/images/I/61yqZqfc1uL._AC_SL1500_.jpg",
+  },
+  {
+    id: uniqid(),
+    name: 'Microsoft Surface Pro 7 12.3" 128GB Windows 10 Tablet',
+    price: 679.99,
+    description:
+      "Create, work, and bring possibilities to life with Microsoft Surface Pro 7. Ideal for commuters and on-the-go professionals, this ultra-slim tablet boasts a 10th generation Intel Core i3 processor and 4GB of RAM. Note: Keyboard, mouse, dial, and pen are sold separately. Free Upgrade to Windows 11 when available ",
+    imgSrc: "https://m.media-amazon.com/images/I/513AZVpI-3L._AC_SL1500_.jpg",
+  },
+  {
+    id: uniqid(),
+    name: 'Lenovo Tab M7 7" 32GB Android Tablet - Grey',
+    price: 79.99,
+    description:
+      'Enjoy portable productivity offered by the Lenovo Tab M7 Android tablet. Equipped with a HD 7" screen, quad-core processor, and high speed LPDDR4 RAM, it offers impressive visuals as well as lag-free performance. In a premium metal design, it features a battery capable of quick charging and offers up to 12 hours of life.',
+    imgSrc: "https://m.media-amazon.com/images/I/71ZinCplXKL._AC_SX679_.jpg",
+  },
+  {
+    id: uniqid(),
+    name: 'Samsung Galaxy Tab S6 Lite 10.4" 64GB Android Tablet - Angora Blue',
+    price: 279.99,
+    description:
+      'Make your life conveniently portable with the slim, lightweight Samsung Galaxy Tab S6 Lite. This powerful 10.4" Android tablet boasts an 8-core Exynos 9611 processor with 4GB of RAM. Its 64GB of built-in storage space gives you plenty of room for all your games, documents, and other files. The included S Pen is perfect for taking notes, drawing, and more.',
+    imgSrc: "https://m.media-amazon.com/images/I/61EobGaoeaL._AC_SL1500_.jpg",
+  },
+  {
+    id: uniqid(),
+    name: 'Apple iPad mini 8.3" 64GB with Wi-Fi (6th Generation) - Pink',
+    price: 649.99,
+    description:
+      "With power beyond its size, there’s a lot to love about the Apple iPad mini. It boasts the A15 Bionic chip, an 8.3-inch Liquid Retina display featuring True Tone, a 12MP Ultra Wide front camera with Center Stage, and Wi-Fi 6 and 5G capability to help you stay connected anywhere. Take notes, mark up documents or capture your biggest ideas with Apple Pencil (2nd generation).",
+    imgSrc: "https://m.media-amazon.com/images/I/71FElasHj3L._AC_SL1500_.jpg",
+  },
+  {
+    id: uniqid(),
+    name: 'Samsung Galaxy Tab S7+ 12.4" 128GB Android Tablet - Mystic Black',
+    price: 1039.99,
+    description:
+      "Whether you're working, gaming, or connecting with friends, you can do it on the go with the Samsung Galaxy Tab S7+ 12.4\" tablet. Featuring 128GB of built-in storage, an eight-core Snapdragon 865+ processor, and 6GB of RAM, it has all the power you need to get any task done. It's equipped with cameras, quad speakers, and the redesigned S Pen for effortless operation.",
+    imgSrc: "https://m.media-amazon.com/images/I/61JJPxH34qS._AC_SL1500_.jpg",
+  },
+  {
+    id: uniqid(),
+    name: 'Apple iPad Air 10.9" 256GB with Wi-Fi (4th Generation) - Space Grey',
+    price: 979.99,
+    description:
+      "The iPad Air does more than a computer in simpler, more magical ways. Featuring the advanced A14 Bionic chip and a stunning 10.9\" Liquid Retina display, and now with more features and capabilities, it's more versatile than ever.",
+    imgSrc: "https://m.media-amazon.com/images/I/719UcXKzXHL._AC_SL1500_.jpg",
+  },
+  {
+    id: uniqid(),
+    name: 'Samsung Galaxy Tab A7 Lite 8.7" 32GB Android Tablet - Mystic Silver',
+    price: 169.99,
+    description:
+      "Take endless entertainment wherever you go with the Samsung Galaxy Tab A7 Lite 8.7\" Android tablet. It's powered by the MediaTek MT8768T 8-Core processor so you can enjoy super-smooth streaming on your tablet with minimal interruptions. With its compact 8.7\" screen, slim design, sturdy metal frame, long battery life, and plenty of storage, it's a perfect device for entertainment on the go.",
+    imgSrc: "https://m.media-amazon.com/images/I/51ady+7SgBS._AC_SL1000_.jpg",
+  },
+  {
+    id: uniqid(),
+    name: 'Microsoft Surface Go 2 10.5" 64GB Windows 10 S Tablet - Platinum',
+    price: 529.99,
+    description:
+      "Take your entertainment or work wherever you go with the lightweight, portable Microsoft Surface Go 2. With a gorgeous 10.5\" PixelSense touchscreen display, this tablet provides crystal-clear visuals in vibrant colours. It's powered by the Intel Pentium Gold 4425Y processor and 4GB of RAM to fuel your apps, games, videos, and much more.",
+    imgSrc: "https://m.media-amazon.com/images/I/71cC6nLXAtL._AC_SL1500_.jpg",
+  },
+  {
+    id: uniqid(),
+    name: 'Lenovo Tab P11 11" 64GB Android 11 Tablet - Slate Grey',
+    price: 249.99,
+    description:
+      'Stream videos and enjoy attractive visuals with the Lenovo P11 11" touchscreen tablet. It offers smooth and efficient performance, thanks to its 2.0 GHz MediaTek HelioG90T processor and 4GB of RAM, while 64GB of internal storage lets you save plenty of data on the device. It also comes with the latest Google Kids Space, featuring content that helps kids discover, create and grow.',
+    imgSrc: "https://m.media-amazon.com/images/I/617Q3DbKyPL._AC_SL1500_.jpg",
+  },
+];
+
+export default tablets;
